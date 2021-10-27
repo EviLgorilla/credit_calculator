fe2da4c4a96aa17db62a2d69889f997a6f0d2060
Mendus <alexander.mendus1@gmail.com>
2021-10-27 20:41:48 +0300

    Fix enum eslint

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { Menu } from "antd";
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
 
--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,14 +1,14 @@
-import {FunctionComponent} from "react";
-import styles from './main-layout.module.css';
-import {Header} from "components/header";
+import { Header } from "components/header";
+import React, { FunctionComponent } from "react";
+import styles from "./main-layout.module.css";
 
-const MainLayout: FunctionComponent = ({children}) => {
+const MainLayout: FunctionComponent = ({ children }) => {
     return (
-        <div className={styles.main_layout}>
+        <div className={ styles.main_layout }>
             <Header />
-            {children}
+            { children }
         </div>
-    )
-}
+    );
+};
 
 export default MainLayout;
